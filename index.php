b786d6dbb50ede162fe9a62c30c527fcbc611afe
christianturner@hotmail.co.uk <academy@lpt00361mac.home>
2020-03-23 15:36:48 +0000

    Task 1d changes ready for review

--- index.php
+++ index.php
@@ -1,41 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Log-in Page!</title>
-</head>
-<body>
-<form action = "code.php" method = "post">
-    E-mail: <input type ="email" name="userInputEmail"><br>
-    Password: <input type ="password" name="userInputPassword"><br>
-    <input type="submit">
-</form>
-Create Account: <br>
-<form action = "createUser.php" method = "post">
-    E-mail: <input type ="email" name="userCreateEmail"><br>
-    Password: <input type ="password" name="userCreatePassword"><br>
-    Re-type Password: <input type ="password" name="userCheckPassword"><br>
-    <input type="submit">
-</form>
-
-</body>
-</html>
 <?php
-if (!empty($_GET['error'])) {
-    echo $_SESSION['error'][$_GET['error']];
-}
-if (!empty($_GET['success'])) {
-    echo $_SESSION['success'][$_GET['success']];
-}
-
-
-
 
 $db = new PDO('mysql:host=DB;dbname=collectionproject', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 $query = $db->query("SELECT * FROM `computerMice`");
-$query = $query->fetchAll();
-var_dump($query);
+$mice = $query->fetchAll();
+
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<body
+    <nav>
+    <h2>Mouse Collection</h2>
+        <a>Collection</a>
+        <a>Coming soon!</a>
+    </nav>
+<main>
+    <?php
+    foreach ($mice as $mouse) {
+        echo '<h6>' . $mouse["name"] . '</h6>';
+        echo '<p>Brand: ' . $mouse["brand"] . '<p>';
+        echo '<p>Weight: ' . $mouse["weight"] . 'g<p>';
+    }
+    ?>
+</main>
+
+
+</body>
+</html>
