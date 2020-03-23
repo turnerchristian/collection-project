2c35c83b49b0ca0b704646c0c5590932c02c9eb2
christianturner@hotmail.co.uk <academy@lpt00361mac.home>
2020-03-23 14:28:00 +0000

    fixed changes

---
+++ collectionproject_2020-03-23.sql
@@ -0,0 +1,56 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.29)
+# Database: collectionproject
+# Generation Time: 2020-03-23 14:25:43 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table computerMice
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `computerMice`;
+
+CREATE TABLE `computerMice` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `brand` varchar(255) NOT NULL DEFAULT '',
+  `weight` smallint(5) DEFAULT NULL,
+  `is_wireless` tinyint(2) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `computerMice` WRITE;
+/*!40000 ALTER TABLE `computerMice` DISABLE KEYS */;
+
+INSERT INTO `computerMice` (`id`, `name`, `brand`, `weight`, `is_wireless`)
+VALUES
+	(1,'Razer Viper','Razer',70,0),
+	(2,'Razer Viper Ultimate','Razer',74,1),
+	(3,'G Pro Wireless','Logitech',80,1);
+
+/*!40000 ALTER TABLE `computerMice` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
