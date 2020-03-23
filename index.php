ada3d00e91059d9729e42ef61643cc49abd9c8cf
christianturner@hotmail.co.uk <academy@lpt00361mac.home>
2020-03-23 15:15:50 +0000

    com

--- index.php
+++ index.php
@@ -1,4 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Log-in Page!</title>
+</head>
+<body>
+<form action = "code.php" method = "post">
+    E-mail: <input type ="email" name="userInputEmail"><br>
+    Password: <input type ="password" name="userInputPassword"><br>
+    <input type="submit">
+</form>
+Create Account: <br>
+<form action = "createUser.php" method = "post">
+    E-mail: <input type ="email" name="userCreateEmail"><br>
+    Password: <input type ="password" name="userCreatePassword"><br>
+    Re-type Password: <input type ="password" name="userCheckPassword"><br>
+    <input type="submit">
+</form>
+
+</body>
+</html>
 <?php
+if (!empty($_GET['error'])) {
+    echo $_SESSION['error'][$_GET['error']];
+}
+if (!empty($_GET['success'])) {
+    echo $_SESSION['success'][$_GET['success']];
+}
+
+
+
+
 $db = new PDO('mysql:host=DB;dbname=collectionproject', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
@@ -6,3 +38,4 @@ $query = $db->query("SELECT * FROM `computerMice`");
 $query = $query->fetchAll();
 var_dump($query);
 
+?>
