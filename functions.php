77806e7ef624c798f433d37515a55499b683706e
christianturner@hotmail.co.uk <academy@lpt00361mac.home>
2020-03-25 14:19:44 +0000

    Task2b, user input validation and resulting inputs done, ready for review

--- functions.php
+++ functions.php
@@ -25,7 +25,31 @@ function linkDB(): PDO
 
 function getDataFromDB(PDO $db): array
 {
-    $query = $db->query("SELECT `name`, `brand`, `image`, `weight` FROM `computerMice` WHERE (`name` IS NOT NULL AND `brand` IS NOT NULL);");
+    $query = $db->query("SELECT `name`, `brand`, `image`, `weight`, `is_wireless` FROM `computerMice` 
+    WHERE (`name` IS NOT NULL AND `brand` IS NOT NULL AND `weight` IS NOT NULL AND `is_wireless` IS NOT NULL);");
     return $query->fetchAll();
 }
+
+function formatName(string $name): string
+{
+    $name = trim($name, " \t\n\r\0\x0B");
+    $name = ucwords($name);
+    return $name;
+}
+
+
+function validateInfo(string $mouseName,string $mouseBrand,string $mouseWeight)
+{
+    if ((strlen($mouseName) > 100) || (strlen($mouseBrand) > 50) || ($mouseWeight > 1000) || ($mouseWeight < 1)) {
+        return 'inputLength';
+    } elseif (!(filter_var($mouseWeight, FILTER_VALIDATE_INT))) {
+        return 'integer';
+    } elseif ((!(preg_match("/^([a-zA-Z0-9- ]+)$/", $mouseName))) || (!(preg_match("/^([a-zA-Z0-9- ]+)$/", $mouseBrand)))) {
+        return 'invalidName';
+    } else {
+        return true;
+    }
+
+}
+
 
--- inputPageCode.php
+++ inputPageCode.php
@@ -1,1 +1,33 @@
 <?php
+require_once('session.php');
+require_once('functions.php');
+
+$db = linkDB();
+$mice = getDataFromDB($db);
+
+if (!empty($_POST['mouseName']) && !empty($_POST['mouseBrand']) && !empty($_POST['mouseWeight']) && !empty($_POST['wirelessInput'])) {
+    $mouseName = $_POST['mouseName'];
+    $mouseBrand = $_POST['mouseBrand'];
+    $mouseWeight = $_POST['mouseWeight'];
+    if ($_POST['wirelessInput'] == 'yes') {
+        $isWireless = 1;
+    } else {
+        $isWireless = 0;
+    }
+} else {
+    header('Location: http://localhost:1234/collection-project/inputPage.php?error=missing');
+    exit;
+}
+var_dump($mouseName);
+var_dump($mouseBrand);
+
+if (validateInfo($_POST['mouseName'], $_POST['mouseBrand'], $_POST['mouseWeight']) !== true) {
+    header('Location: http://localhost:1234/collection-project/inputPage.php?error=' . validateInfo($_POST['mouseName'], $_POST['mouseBrand'], $_POST['mouseWeight'], $_POST['wirelessInput']));
+    exit;
+} else {
+    $mouseName = formatName($mouseName);
+    $mouseBrand = formatName($mouseBrand);
+}
+var_dump($mouseName);
+var_dump($mouseBrand);
+
